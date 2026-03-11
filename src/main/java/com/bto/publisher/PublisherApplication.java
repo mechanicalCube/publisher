33aa39519cdd037e15114da2f50d21258fecf0bf
BetiSu <zurawska.qa@gmail.com>
2026-03-11 18:51:17 +0100

    Lekcja 31

--- src/main/java/com/bto/publisher/PublisherApplication.java
+++ src/main/java/com/bto/publisher/PublisherApplication.java
@@ -1,13 +1,20 @@
 package com.bto.publisher;
 
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PublisherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PublisherApplication.class, args);
+	}
+
+	@Bean
+	public JacksonJsonMessageConverter jsonMessageConverter() {
+		return new JacksonJsonMessageConverter();
 	}
 
 }
--- src/main/java/com/bto/publisher/controller/MessageController.java
+++ src/main/java/com/bto/publisher/controller/MessageController.java
@@ -1,6 +1,6 @@
-package com.bto.publisher;
+package com.bto.publisher.controller;
 
-import com.bto.notification.Notification;
+import com.bto.publisher.model.Notification;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.*;
 
